244867293da120883898833e83cf769864a5e28f
Ansagan Murat <ansaganmurat29@gmail.com>
2026-01-19 00:45:25 +0500

    repository

---
+++ Appointment_booking_system/src/main/java/repository/UserRepository.java
@@ -0,0 +1,57 @@
+package repository;
+
+import config.interfaces.IDB;
+import entity.User;
+import repository.interfaces.IUserRepository;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserRepository implements IUserRepository{
+    private final IDB db;
+
+    public UserRepository(IDB db) {
+        this.db = db;
+    }
+
+    @Override
+    public boolean createUser(User user) {
+        String sql = "INSERT INTO users(name, surname, gender) VALUES(?, ?, ?)";
+        try (Connection con = db.getConnection();
+             PreparedStatement st = con.prepareStatement(sql)) {
+
+            st.setString(1, user.getName());
+            st.setString(2, user.getSurname());
+            st.setBoolean(3, user.getGender()); // Используем setBoolean
+
+            st.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            System.out.println("SQL Error: " + e.getMessage());
+            return false;
+        }
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        List<User> users = new ArrayList<>();
+        String sql = "SELECT id, name, surname, gender FROM users";
+
+        try (Connection con = db.getConnection();
+             Statement st = con.createStatement();
+             ResultSet rs = st.executeQuery(sql)) {
+
+            while (rs.next()) {
+                users.add(new User(
+                        rs.getInt("id"),
+                        rs.getString("name"),
+                        rs.getString("surname"),
+                        rs.getBoolean("gender") // Используем getBoolean
+                ));
+            }
+        } catch (SQLException e) {
+            System.out.println("SQL Error: " + e.getMessage());
+        }
+        return users;
+    }
+}
